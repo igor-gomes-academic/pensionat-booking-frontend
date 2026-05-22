b5d3a4e9c8343b15530f05def3f996ac16d93284
Daniel Lyytikäinen <daniel.lyytikainen@yh.nackademin.se>
2026-05-22 10:56:11 +0200

    added account button to Navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -18,18 +18,22 @@ export default function Navbar() {
   };
 
   return (
-    <nav style={{ display: "flex", justifyContent: "space-between", padding: "1rem", backgroundColor: "#333" }}>
-      <h1 style={{ color: "white" }}>Pensionat</h1>
+    <nav style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "1rem", backgroundColor: "#333" }}>
+      <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+        <h1 style={{ color: "white", margin: 0 }}>Pensionat</h1>
+        <a href="/" style={{ color: "white", textDecoration: "none", border: "1px solid white", padding: "0.3rem 1rem", cursor: "pointer" }}>Home</a>
+      </div>
       <div>
         {customer ? (
           <>
             <span style={{ color: "white", marginRight: "1rem" }}>Hello, {customer.firstName}!</span>
+            <a href="/account" style={{ color: "white", textDecoration: "none", marginRight: "3rem", border: "1px solid white", padding: "0.3rem 1rem", cursor: "pointer", display: "inline-block" }}>Account</a>
             <button onClick={handleLogout} style={{ color: "white", background: "none", border: "1px solid white", padding: "0.3rem 1rem", cursor: "pointer" }}>
               Logout
             </button>
           </>
         ) : (
-          <a href="/login" style={{ color: "white", textDecoration: "none" }}>Login</a>
+          <a href="/login" style={{ color: "white", textDecoration: "none", border: "1px solid white", padding: "0.3rem 1rem", cursor: "pointer",display: "inline-block" }}>Login</a>
         )}
       </div>
     </nav>
