d35e83d14c6615f10570a2ddbdc0ef19c7d15090
niklasdahlen <dahlstromniklas1@gmail.com>
2026-05-27 12:44:30 +0200

    feature/footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,20 +1,19 @@
 export default function Footer() {
     return (
         <footer 
-        style= {{
+        style={{
             backgroundColor: "#2f4156",
+            color: "white",
             textAlign: "center",
             padding: "1.5rem",
-            boxShadow: "0 - 2px 10px rgba(0,0,0,0.15)",
-             
-        }}  > 
-
+            boxShadow: "0 -2px 10px rgba(0,0,0,0.15)",
+        }}> 
          <p style={{ margin: 0 }}>
-        © {new Date().getFullYear()} Ocean View Lodge
-      </p>
-      <p style={{ margin: "0.5rem 0 0", opacity: 0.85 }}>
-        {"Created by Daniel, Igor, Niklas, Patric"}
-      </p>
+            © {new Date().getFullYear()} Ocean View Lodge
+         </p>
+         <p style={{ margin: "0.5rem 0 0", opacity: 0.85 }}>
+            Created by Daniel, Igor, Niklas, Patric
+         </p>
     </footer>
     )
 }
